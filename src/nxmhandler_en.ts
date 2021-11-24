73183880ed689a953bc769b37ce5470454a0b23e
Chris Bessent <lost.dragonist@gmail.com>
2021-11-23 20:58:18 -0700

    Update translations

---
+++ src/nxmhandler_en.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AddBinaryDialog</name>
+    <message>
+        <location filename="addbinarydialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.ui" line="24"/>
+        <source>Select Supported Games</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.ui" line="42"/>
+        <source>Select Handler Binary (i.e. ModOrganizer.exe)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.ui" line="60"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.ui" line="69"/>
+        <source>Set Arguments (optional)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.cpp" line="51"/>
+        <source>Select Executable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.cpp" line="52"/>
+        <source>Executable (*.exe)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HandlerWindow</name>
+    <message>
+        <location filename="handlerwindow.ui" line="14"/>
+        <source>NXM Handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="21"/>
+        <source>Use this list to configure programs to handle nxm links. Different Programs can be set up to handle links for different games. If the same game is supported by multiple binaries, the top-most is used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="50"/>
+        <source>Supported Games</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="55"/>
+        <source>Binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="60"/>
+        <source>Arguments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="70"/>
+        <source>Primary Handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="84"/>
+        <source>Register active</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="108"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="122"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="125"/>
+        <source>Remove Binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="128"/>
+        <source>Del</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="137"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="140"/>
+        <source>Add a binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.cpp" line="37"/>
+        <location filename="handlerwindow.cpp" line="124"/>
+        <source>&lt;Current&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.cpp" line="119"/>
+        <source>Change handler registration?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.cpp" line="120"/>
+        <source>This will make the nxmhandler.exe you called the primary handler registered in the system.
+That has no immediate impact on how links are handled.
+Use this if you moved Mod Organizer or if you uninstalled the Mod Organizer installation that was previously registered. Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="124"/>
+        <source>Change Handler?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="125"/>
+        <source>A nxm handler from a different Mod Organizer installation has been registered. Do you want to replace it? This is usually not necessary unless the other installation is defective.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="136"/>
+        <source>Register?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="137"/>
+        <source>Mod Organizer is not set up to handle nxm links. Associate it with nxm links?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="141"/>
+        <source>No, don&apos;t ask again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="250"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="250"/>
+        <location filename="main.cpp" line="271"/>
+        <source>Invalid number of parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="262"/>
+        <source>No handler found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="263"/>
+        <source>No application registered to handle this game (%1).
+If you expected Mod Organizer to handle the link, you have to go to Settings-&gt;Nexus and click the &quot;Associate with ... links&quot;-button.
+If you have NMM installed, you can re-register it for nxm-links so it handles the links that MO doesn&apos;t.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="271"/>
+        <source>Invalid Arguments</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="290"/>
+        <source>Uncaught exception:
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
