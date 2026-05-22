5fa9a8112903f6b6aab48c93de5dc198615d8e5d
Jeremy Rimpo <jrim@rimpo.org>
2026-05-21 23:13:50 -0500

    Translation file updates

--- src/nxmhandler_en.ts
+++ src/nxmhandler_en.ts
@@ -25,16 +25,21 @@
     </message>
     <message>
         <location filename="addbinarydialog.ui" line="69"/>
+        <source>Assigned Schema</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="addbinarydialog.ui" line="79"/>
         <source>Set Arguments (optional)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="addbinarydialog.cpp" line="51"/>
+        <location filename="addbinarydialog.cpp" line="61"/>
         <source>Select Executable</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="addbinarydialog.cpp" line="52"/>
+        <location filename="addbinarydialog.cpp" line="61"/>
         <source>Executable (*.exe)</source>
         <translation type="unfinished"></translation>
     </message>
@@ -43,83 +48,107 @@
     <name>HandlerWindow</name>
     <message>
         <location filename="handlerwindow.ui" line="14"/>
-        <source>NXM Handler</source>
+        <source>MO2 Download Handler</source>
+        <oldsource>NXM Handler</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="handlerwindow.ui" line="21"/>
-        <source>Use this list to configure programs to handle nxm links. Different Programs can be set up to handle links for different games. If the same game is supported by multiple binaries, the top-most is used.</source>
+        <source>Use this list to configure programs to handle download links. Different programs can be set up to handle links for different games. If the same game is supported by multiple binaries, the top-most is used.</source>
+        <oldsource>Use this list to configure programs to handle nxm links. Different Programs can be set up to handle links for different games. If the same game is supported by multiple binaries, the top-most is used.</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="50"/>
+        <location filename="handlerwindow.ui" line="53"/>
         <source>Supported Games</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="55"/>
+        <location filename="handlerwindow.ui" line="58"/>
+        <source>Schema Handler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="63"/>
         <source>Binary</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="60"/>
+        <location filename="handlerwindow.ui" line="68"/>
         <source>Arguments</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="70"/>
-        <source>Primary Handler</source>
+        <location filename="handlerwindow.ui" line="78"/>
+        <source>Current NXM Handler</source>
+        <oldsource>Primary Handler</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="84"/>
+        <location filename="handlerwindow.ui" line="92"/>
+        <location filename="handlerwindow.ui" line="113"/>
         <source>Register active</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="108"/>
-        <source>Close</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="handlerwindow.ui" line="122"/>
-        <source>Remove</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="handlerwindow.ui" line="125"/>
-        <source>Remove Binary</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="handlerwindow.ui" line="128"/>
-        <source>Del</source>
+        <location filename="handlerwindow.ui" line="103"/>
+        <source>Current MODL Handler</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="handlerwindow.ui" line="137"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="151"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="154"/>
+        <source>Remove Binary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="157"/>
+        <source>Del</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.ui" line="166"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.ui" line="140"/>
+        <location filename="handlerwindow.ui" line="169"/>
         <source>Add a binary</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.cpp" line="37"/>
-        <location filename="handlerwindow.cpp" line="124"/>
+        <location filename="handlerwindow.cpp" line="38"/>
+        <location filename="handlerwindow.cpp" line="48"/>
+        <location filename="handlerwindow.cpp" line="145"/>
+        <location filename="handlerwindow.cpp" line="162"/>
         <source>&lt;Current&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.cpp" line="119"/>
+        <location filename="handlerwindow.cpp" line="138"/>
+        <location filename="handlerwindow.cpp" line="155"/>
         <source>Change handler registration?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="handlerwindow.cpp" line="120"/>
-        <source>This will make the nxmhandler.exe you called the primary handler registered in the system.
+        <location filename="handlerwindow.cpp" line="139"/>
+        <source>This will make the nxmhandler.exe you called the NXM handler registered in the system.
+That has no immediate impact on how links are handled.
+Use this if you moved Mod Organizer or if you uninstalled the Mod Organizer installation that was previously registered. Continue?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="handlerwindow.cpp" line="156"/>
+        <source>This will make the nxmhandler.exe you called the MODL handler registered in the system.
 That has no immediate impact on how links are handled.
 Use this if you moved Mod Organizer or if you uninstalled the Mod Organizer installation that was previously registered. Continue?</source>
         <translation type="unfinished"></translation>
@@ -128,60 +157,68 @@ Use this if you moved Mod Organizer or if you uninstalled the Mod Organizer inst
 <context>
     <name>QObject</name>
     <message>
-        <location filename="main.cpp" line="125"/>
+        <location filename="main.cpp" line="159"/>
         <source>Change Handler?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="126"/>
-        <source>A nxm handler from a different Mod Organizer installation has been registered. Do you want to replace it? This is usually not necessary unless the other installation is defective.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.cpp" line="137"/>
+        <location filename="main.cpp" line="172"/>
         <source>Register?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="138"/>
-        <source>Mod Organizer is not set up to handle nxm links. Associate it with nxm links?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.cpp" line="142"/>
+        <location filename="main.cpp" line="177"/>
         <source>No, don&apos;t ask again</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="251"/>
+        <location filename="main.cpp" line="160"/>
+        <source>A %1 handler from a different Mod Organizer installation has been registered. Do you want to replace it? This is usually not necessary unless the other installation is defective.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="173"/>
+        <source>Mod Organizer is not set up to handle %1 links. Associate it with %1 links?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="323"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="251"/>
-        <location filename="main.cpp" line="272"/>
+        <location filename="main.cpp" line="324"/>
+        <location filename="main.cpp" line="387"/>
         <source>Invalid number of parameters</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="263"/>
+        <location filename="main.cpp" line="337"/>
+        <location filename="main.cpp" line="377"/>
         <source>No handler found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="264"/>
+        <location filename="main.cpp" line="338"/>
         <source>No application registered to handle this game (%1).
 If you expected Mod Organizer to handle the link, you have to go to Settings-&gt;Nexus and click the &quot;Associate with ... links&quot;-button.
 If you have NMM installed, you can re-register it for nxm-links so it handles the links that MO doesn&apos;t.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="272"/>
+        <location filename="main.cpp" line="378"/>
+        <source>No application registered to handle this game (%1).
+If you expected Mod Organizer to handle the link, you have to go to Settings and click the &quot;Associate with MODL links&quot;-button.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="386"/>
         <source>Invalid Arguments</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.cpp" line="291"/>
+        <location filename="main.cpp" line="412"/>
         <source>Uncaught exception:
 %1</source>
         <translation type="unfinished"></translation>
